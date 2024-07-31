004c258c271b07aea49d4637f62910cc54f884d0
davifeliciano3 <feeliciaanoo@gmail.com>
2024-07-30 21:18:21 -0300

    Testando o commit

---
+++ funcoes/funcoes_DocStrings.py
@@ -0,0 +1,15 @@
+"""
+Documentando funções com Docstrings
+
+OBS:Podemos ter acesso á documentação de uma função em Python
+utilizando a propriedade especial __doc__ - diz_oi.__doc__
+
+Podemos fazer acesso com a função help() help(diz_oi)
+"""
+
+def diz_oi():
+    """
+    Uma função que retorna oi
+    :return:
+    """
+    return 'oi'
---
+++ funcoes/funcoes_parâmetro_padrão.py
@@ -0,0 +1,11 @@
+"""
+Funções com parâmetro padrão Default Paramters
+def expoente(num,exp=2): #se não passar o expoente por padrão será dois
+    return num ** exp #O parametro padrão sempre devera ser passado depois do obrigatorio
+
+expoente(exp=1,3) acesso mudando a ordem
+
+
+Para usar uma variavel global dentro de uma função utiliza-se global 'nome da variavel'
+Para usar uma variavel que esta dentro de uma função em uma função dentro da mesma utiliza-se  nonlocal
+"""
