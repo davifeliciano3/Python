16f13778ec9eb012f162e12d22a23df1568d6185
davifeliciano3 <davifeliciano997@gmail.com>
2024-07-30 22:34:29 -0300

    kwargs

---
+++ funcoes/funcoes_args.py
@@ -0,0 +1,10 @@
+"""
+O *args é um parâmetro, como outro qualquer. Isso significa que voçê poderá chamar de qualquer coisa
+desde que começe com asterisco
+
+O parâmetro *args utilizado em uma função, coloca os valores extras informados como entrada em uma tupla
+Então desde já lembre-se que tuplas são imutáveis
+
+O sinal de * também serve para desempacotar uma lista
+
+"""
---
+++ funcoes/funcoes_kwargs.py
@@ -0,0 +1,21 @@
+"""
+**Kwargs
+Este é só mais um parâmetro, mas diferente do *args que coloca os valores extras em uma tupla, o **Kwargs
+exige que utilizemos parâmetros nomeados, e transforma esses parâmetros extras em um dicionário
+Vc tem que passar uma chave:valor
+
+
+##Os parametros *args **Kwargs não são obrigatórios
+
+Existe uma ordem para passar os parâmetros
+1-Parâmetros obrigatórios
+2-*args
+3-Parametros default (não obrigatórios)
+4-**Kwargs
+
+
+** desempacota o dicionário
+* desempacota lista,tupla e conjunto
+
+os nomes das chaves em um dicionário devem ser o mesmo dos para^metros da função
+"""
